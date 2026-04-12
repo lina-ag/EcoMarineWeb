cd721b2a68d3fcd39986107a178ef936b3c7018e
Med amine <medaminesayadi101@gmail.com>
2026-04-12 23:02:47 +0100

    ajout gestion action nettoyage et volontaire

--- src/Form/VolontaireType.php
+++ src/Form/VolontaireType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\ActionNettoyage;
 use App\Entity\Volontaire;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,29 @@ class VolontaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('contact')
-            ->add('id_action')
-        ;
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrer le nom du volontaire',
+                    'maxlength' => 100,
+                ],
+            ])
+            ->add('contact', TextType::class, [
+                'label' => 'Contact',
+                'attr' => [
+                    'placeholder' => 'Entrer le contact',
+                    'maxlength' => 20,
+                ],
+            ])
+            ->add('id_action', EntityType::class, [
+                'class' => ActionNettoyage::class,
+                'choice_label' => function (ActionNettoyage $action) {
+                    $date = $action->getDateAction() ? $action->getDateAction()->format('Y-m-d') : '';
+                    return $action->getLieu() . ' - ' . $date;
+                },
+                'placeholder' => 'Choisir une action de nettoyage',
+                'label' => 'Action de nettoyage',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
