4cfc1760704d273ced7a8de9a63f3f468b9af2c6
lina-ag <lina.aguir@esprim.tn>
2026-04-12 18:25:49 +0100

    formulaire reservation front + modification dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Form\ReservationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +13,12 @@ final class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $reservation = new Reservation();
+        $formReservation = $this->createForm(ReservationType::class, $reservation);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form_reservation' => $formReservation,
         ]);
     }
 }
