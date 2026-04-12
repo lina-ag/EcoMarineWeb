d5c24ade37c7dd8782df974747c51e5b5873d145
lina-ag <lina.aguir@esprim.tn>
2026-04-12 18:39:24 +0100

    Fix home reservation form variable and imports

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Form\ReservationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +14,11 @@ final class HomeController extends AbstractController
     #[Route('/home', name: 'app_home', methods: ['GET', 'POST'])]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $formReservation = $this->createForm(ReservationType::class);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form_reservation' => $formReservation->createView(),
         ]);
     }
 }
