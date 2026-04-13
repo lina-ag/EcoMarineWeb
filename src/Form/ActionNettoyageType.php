d8b66916963ef3222c0987d82bcdbe4d6e968a9f
unknown <teyssirhouma@gmail.com>
2026-04-13 02:17:16 +0200

    Ajout du système d'authentification : inscription et connexion

--- src/Form/ActionNettoyageType.php
+++ src/Form/ActionNettoyageType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\ActionNettoyage;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,24 @@ class ActionNettoyageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_action')
-            ->add('lieu')
+            ->add('date_action', DateType::class, [
+                'label' => 'Date de l’action',
+                'widget' => 'single_text',
+            ])
+            ->add('lieu', TextType::class, [
+                'label' => 'Lieu',
+                'attr' => [
+                    'placeholder' => 'Entrer le lieu de l’action',
+                    'maxlength' => 255,
+                ],
+            ])
+            ->add('limiteBenevoles', IntegerType::class, [
+                'label' => 'Limite de bénévoles',
+                'attr' => [
+                    'placeholder' => 'Entrer la limite',
+                    'min' => 1,
+                ],
+            ])
         ;
     }
 
