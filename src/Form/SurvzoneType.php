f29ff7ffdb261c5e91d4f4776ef4bbf64f259221
lina-ag <lina.aguir@esprim.tn>
2026-04-15 17:14:21 +0100

    ajout recurrence

--- src/Form/SurvzoneType.php
+++ src/Form/SurvzoneType.php
@@ -8,10 +8,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\LessThanOrEqual;
-use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 
@@ -23,17 +19,6 @@ class SurvzoneType extends AbstractType
             ->add('dateSurv', DateType::class, [
                 'widget' => 'single_text',
                 'label' => 'Date de surveillance',
-                'constraints' => [
-                    new NotBlank(['message' => 'La date est obligatoire']),
-                    new LessThanOrEqual([
-                        'value' => new \DateTime('today'),
-                        'message' => 'La date ne peut pas être dans le futur',
-                    ]),
-                    new GreaterThanOrEqual([
-                        'value' => new \DateTime('-10 years'),
-                        'message' => 'La date ne peut pas être antérieure à 10 ans',
-                    ]),
-                ],
             ])
 
             ->add('observation', TextareaType::class, [
@@ -44,13 +29,6 @@ class SurvzoneType extends AbstractType
         'placeholder' => 'Entrez vos observations ici...',
         'maxlength' => 1000,
     ],
-    'constraints' => [
-        new NotBlank(['message' => 'L\'observation est obligatoire']),
-        new Length([
-            'max' => 1000,
-            'maxMessage' => 'L\'observation ne peut pas dépasser {{ limit }} caractères',
-        ]),
-    ],
 ])
 
             ->add('zone', EntityType::class, [
@@ -58,9 +36,6 @@ class SurvzoneType extends AbstractType
                 'choice_label' => 'nomZone',
                 'placeholder' => 'Choisir une zone',
                 'label' => 'Zone',
-                'constraints' => [
-                    new NotBlank(['message' => 'La zone est obligatoire']),
-                ],
             ])
         ;
     }
